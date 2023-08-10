5c15fa4a0d8bc7d3ce0e92cdbbde238ecbc7afc1
Pavlo Karas <kar.pavel123@gmail.com>
2023-08-10 16:11:29 +0300

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  //import nav menu
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a class="b-5 p-5" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
