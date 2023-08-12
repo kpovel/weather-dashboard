269a5875a96036a1709f4b5a20e5ff421314b9e0
Pavlo Karas <kar.pavel123@gmail.com>
2023-08-12 22:44:33 +0300

    Created weather fetching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,43 @@
 <script>
-  //import nav menu
+  //import { OPENWEATHERMAP_KEY } from "$env/static/private";
+
+  let location = '';
+  /** @type {import('./$types').WeatherData} */
+  let weatherData;
+
+  async function fetchCurrentWeather() {
+    // todo: get a weather token fron env using sveltekit
+    const weatherToken = 'token';
+    const url = 'https://api.openweathermap.org/data/2.5/weather';
+    const api = `${url}?q=${location}&appid=${weatherToken}`;
+
+    const promise = await fetch(api);
+    const json = await promise.json();
+    console.log(json);
+    weatherData = json;
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a class="b-5 p-5" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<form
+  on:submit|preventDefault={() => {
+    fetchCurrentWeather();
+    location = '';
+  }}
+  action="submit"
+>
+  <label>
+    <span class="block font-medium text-slate-700 after:ml-0.5 after:text-red-500">
+      Weather to search:
+    </span>
+    <input
+      bind:value={location}
+      class="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+      placeholder="Enter a city name"
+    />
+    <button type="submit">Find current weather</button>
+  </label>
+</form>
+
+{#if weatherData}
+  {weatherData.weather[0].main}
+{/if}
