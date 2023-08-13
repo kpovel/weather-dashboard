ba707d47a82d0f0019e3b3e4cfa56b347bfc699c
Pavlo Karas <kar.pavel123@gmail.com>
2023-08-13 23:12:31 +0300

    Enhanced design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   //import { OPENWEATHERMAP_KEY } from "$env/static/private";
   import { getCurrentPosition } from './getCurrentPosition';
+  import Mappin from '../img/map-pin.svg';
+  import Map from '../img/map.svg';
 
   let location = '';
   const weatherData = {
@@ -78,19 +80,29 @@
   }}
   action="submit"
 >
-  <label>
-    <span class="block font-medium text-slate-700 after:ml-0.5 after:text-red-500">
+  <label class="mx-auto flex w-[90vw] flex-col gap-2 text-sm font-semibold text-gray-900">
+    <span class="mt-2 text-xl">
       Weather to search:
     </span>
     <input
       bind:value={location}
-      class="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
-      placeholder="Enter a city name"
+      class="w-full rounded-md required:border-red-500 ring-1 ring-inset ring-gray-300 bg-white px-3 py-2 placeholder-slate-500 shadow-sm focus:outline-none focus:ring-gray-400"
+      placeholder="Enter a location"
     />
-    <button type="submit">Find current weather</button>
-    <button on:click|preventDefault={fetchWeatherByCurrentPosition}
-      >Find weather by current position</button
+    <button
+      type="submit"
+      class="inline-flex items-center rounded-md bg-white px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
     >
+      <img src={Map} alt="Map" class="mr-1.5 h-5 w-5 shrink-0" />
+      Find current weather
+    </button>
+    <button
+      on:click|preventDefault={fetchWeatherByCurrentPosition}
+      class="inline-flex items-center rounded-md bg-white px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    >
+      <img src={Mappin} alt="Map pin" class="mr-1.5 h-5 w-5 flex-shrink-0" />
+      Use my location
+    </button>
   </label>
 </form>
 
