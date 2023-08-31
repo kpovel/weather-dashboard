175fba2030b61a63fcd7eef0a555799a4882f404
Pavlo Karas <kar.pavel123@gmail.com>
2023-08-31 22:47:45 +0300

    feat: add view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
+  import { onNavigate } from '$app/navigation';
   import '../app.css';
   import Nav from './Nav.svelte';
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((res) => {
+      document.startViewTransition(async () => {
+        res();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 <Nav />
